9c3c65e6fe79cd5d0c59652299f10f879273038a
Paul Yates <paul.maxyat@gmail.com>
2018-11-19 11:51:14 -0500

    vim setup and etc

---
+++ other_config_stuff.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#never use gedit, always use vim
+sudo sed -i 's/gedit/vim/' /usr/share/applications/defaults.list
---
+++ vim_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#COMMENT OUT EVERYTHING YOU DON'T NEED/WANT
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+
+#syntastic
+git clone https://github.com/vim-syntastic/syntastic.git
+
+#lightline
+git clone https://github.com/itchyny/lightline.vim.git
+
+#NERD commenter
+git clone https://github.com/scrooloose/nerdcommenter.git
+
+#Conflicted
+git clone https://github.com/christoomey/vim-conflicted.git
+
+
