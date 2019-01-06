2d2df21a20a5b1f191d67c51790172f14205fb83
Paul Yates <paul.yates@m2mdtech.com>
2019-01-06 17:56:55 -0500

    i3status

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,24 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 compton="$HOME/.config/compton.conf"
 git="$HOME/.gitconfig"
 i3="$HOME/.config/i3/config"
+i3status="$HOME/.config/i3status/config"
 tmux="$HOME/.tmux.conf"
 vim="$HOME/.vimrc"
 zsh="$HOME/.zshrc"
-mkdir ~/.config/
+if [ ! -d ~/.config/ ]; then
+    mkdir ~/.config/
+fi
 mv $compton $compton.bak
 ln -s $dir/compton.conf $compton
 mv $git $git.bak
 ln -s $dir/gitconfig $git
-mkdir ~/.config/i3/
+if [ ! -d ~/.config/i3/ ]; then
+    mkdir ~/.config/i3/
+fi
 mv $i3 $i3.bak
 ln -s $dir/i3-config $i3
+mv $i3status $i3status.bak
+ln -s $dir/i3status-config $i3status
 mv $tmux $tmux.bak
 ln -s $dir/tmux.conf $tmux
 mv $vim $vim.bak
