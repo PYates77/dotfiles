9c6f3e9f23a4063ce9fc8edf1c4d62f6df1d4636
Paul Yates <paul.yates@m2mdtech.com>
2019-01-06 23:22:51 -0500

    bootstrap changes and polybar

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install locations of config files
 compton="$HOME/.config/compton.conf"
 git="$HOME/.gitconfig"
 i3="$HOME/.config/i3/config"
 i3status="$HOME/.config/i3status/config"
+polybar="$HOME/.config/polybar/config"
 tmux="$HOME/.tmux.conf"
 vim="$HOME/.vimrc"
 zsh="$HOME/.zshrc"
-if [ ! -d ~/.config/ ]; then
-    mkdir ~/.config/
+
+# COMPTON
+cp -Lr $compton $compton.bak
+ln -sf $dir/compton.conf $compton
+
+# GIT
+cp -Lr $git $git.bak
+ln -sf $dir/gitconfig $git
+
+# I3
+if [ ! -d $(dirname $i3) ]; then
+    mkdir -p $(dirname $i3)
 fi
-mv $compton $compton.bak
-ln -s $dir/compton.conf $compton
-mv $git $git.bak
-ln -s $dir/gitconfig $git
-if [ ! -d ~/.config/i3/ ]; then
-    mkdir ~/.config/i3/
+cp -Lr $i3 $i3.bak
+ln -sf $dir/i3-config $i3
+
+# I3-STATUS
+if [ ! -d $(dirname $i3status) ]; then
+    mkdir -p $(dirname $i3status)
 fi
-mv $i3 $i3.bak
-ln -s $dir/i3-config $i3
-mv $i3status $i3status.bak
-ln -s $dir/i3status-config $i3status
-mv $tmux $tmux.bak
-ln -s $dir/tmux.conf $tmux
-mv $vim $vim.bak
-ln -s $dir/vimrc $vim
-mv $zsh $zsh.bak
-ln -s $dir/zshrc $zsh
+cp -Lr $i3status $i3status.bak
+ln -sf $dir/i3status-config $i3status
+
+# POLYBAR
+if [ ! -d $(dirname $polybar) ]; then
+    mkdir -p $(dirname $polybar)
+fi
+cp -Lr $polybar $polybar.bak
+ln -sf $dir/polybar-config $polybar
+
+# TMUX
+cp -Lr $tmux $tmux.bak
+ln -sf $dir/tmux.conf $tmux
+
+# VIM
+cp -Lr $vim $vim.bak
+ln -sf $dir/vimrc $vim
+
+# ZSH
+cp -Lr $zsh $zsh.bak
+ln -sf $dir/zshrc $zsh
