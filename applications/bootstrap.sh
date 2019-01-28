5f7d21a86799cf0dd2b08535681dceb718484027
Paul Yates <paul.yates@m2mdtech.com>
2019-01-28 00:23:43 -0500

    adding xautolock lights on

---
+++ applications/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install locations of config files
+xautolock_lights_on="/usr/local/bin"
+
+install_file()
+{
+    file=$1; shift
+    path=$1;
+
+    if [ ! -d $(dirname $path) ]; then
+        mkdir -p $(dirname $path)
+    fi
+    cp -Lr ${path}/${file} ${path}/${file}.bak
+    ln -sf $dir/$file $path
+
+}
+
+install_file xautolock_lights_on.sh $xautolock_lights_on
