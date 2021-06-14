138ea950427389afbe8f5739bd00de981978e519
Paul Yates <paul@bitflip.com>
2021-06-14 10:38:10 -0600

    new scripts and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Paul's Checklist for a Happy Environment 
+**DON'T BE A DUMMY, DOUBLE-CHECK THE SCRIPTS BEFORE YOU RUN THEM**
+
+* Install all the basic utilities (install\_tools.sh)
+* Install vim with clipboard and mouse support, install pathogen and plugins, install vimrc (vim\_setup.sh)
+* Install zsh and oh-my-zsh (zsh\_setup.sh)
+* Log into firefox
---
+++ install_tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+package=""
+package+="autoconf autotools-dev libtool "
+package+="build-essential "
+package+="cmake "
+package+="curl "
+package+="git "
+package+="repo "
+package+="adb "
+package+="ctags "
+package+="doxygen "
+package+="openssh-client openssh-server "
+package+="tmux "
+package+="minicom "
+package+="tree "
+package+="xclip "
+package+="zsh "
+package+="vim-gtk "
+
+echo "installing $package"
+#sudo apt-get install $package
